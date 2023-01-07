3958f202a5df186bc8bafef94e81494b081bfa3c
DEATHTINYZ <78017906+DEATHTINYZ@users.noreply.github.com>
2023-01-08 01:48:49 +0700

    Update Pagination.tsx

--- components/pagination/Pagination.tsx
+++ components/pagination/Pagination.tsx
@@ -15,14 +15,14 @@ const Pagination = ({ pageNumber, info, updatePageNumber }: Props) => {
     updatePageNumber(data.selected + 1)
   }
 
-  const [width, setWidth] = useState(window.innerWidth)
-  const updateDimensions = () => {
-    setWidth(window.innerWidth)
-  }
-  useEffect(() => {
-    window.addEventListener('resize', updateDimensions)
-    return () => window.removeEventListener('resize', updateDimensions)
-  }, [])
+  // const [width, setWidth] = useState(window.innerWidth)
+  // const updateDimensions = () => {
+  //   setWidth(window.innerWidth)
+  // }
+  // useEffect(() => {
+  //   window.addEventListener('resize', updateDimensions)
+  //   return () => window.removeEventListener('resize', updateDimensions)
+  // }, [])
 
   return (
     <div className="flex justify-center">
@@ -34,8 +34,8 @@ const Pagination = ({ pageNumber, info, updatePageNumber }: Props) => {
         previousClassName="bg-[#ecd06f] text-white border-2 border-gray-300 bg-white text-center w-[2.5rem] h-[2.5rem] rounded-[50%] flex justify-center items-center"
         nextClassName="bg-[#ecd06f] text-white border-2 border-gray-300 bg-white text-center w-[2.5rem] h-[2.5rem] rounded-[50%] flex justify-center items-center"
         activeClassName="bg-[#ecd06f] text-white border-2 border-gray-300 bg-white text-center w-[2.5rem] h-[2.5rem] rounded-[50%] flex justify-center items-center hover:bg-[#ecd06f]"
-        marginPagesDisplayed={width < 576 ? 1 : 2}
-        pageRangeDisplayed={width < 576 ? 1 : 2}
+        marginPagesDisplayed={100 < 576 ? 1 : 2}
+        pageRangeDisplayed={100 < 576 ? 1 : 2}
         pageCount={info?.pages || 1}
         onPageChange={pageChange}
         pageClassName="border-2 border-gray-300 bg-white text-center w-[2.5rem] h-[2.5rem] rounded-[50%] flex justify-center items-center hover:bg-[gray]"
