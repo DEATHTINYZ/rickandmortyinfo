11202f7edc9dd83132b7fabef3ffe00611369a50
DEATHTINYZ <78017906+DEATHTINYZ@users.noreply.github.com>
2023-01-07 23:08:32 +0700

    Create Option.tsx

---
+++ components/filter/Option.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+interface Props {
+  name: string
+  changeID: (value: number) => void
+  total: number
+}
+
+const Option = ({ name, changeID, total }: Props) => {
+  return (
+    <select
+      onChange={e => changeID(parseInt(e.target.value))}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+      id={name}
+    >
+      <option value="1" disabled>
+        Choose...
+      </option>
+      {[...Array(total).keys()].map((x, index) => {
+        return (
+          <option value={x + 1}>
+            {name} - {x + 1}
+          </option>
+        )
+      })}
+    </select>
+  )
+}
+
+export default Option
